6ec4b241595917fc0d8e9fd19ddaaf0e544f8882
denysnovoascmspain <denys.novoa@scmspain.com>
2015-03-22 14:16:36 +0100

    Add Spark Web Framework and Freemarker Template Engine

--- src/main/java/com/mongodb/HelloWordFreemarkerStyle.java
+++ src/main/java/com/mongodb/HelloWordFreemarkerStyle.java
@@ -1,7 +1,36 @@
 package com.mongodb;
 
-/**
- * Created by denys.novoa on 22/3/15.
- */
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class HelloWordFreemarkerStyle {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloWordFreemarkerStyle.class, "/");
+
+        try {
+            Template helloTemplate = configuration.getTemplate("hello.ftl");
+            StringWriter stringWriter = new StringWriter();
+
+            Map<String, Object> helloMap = new HashMap<String, Object>();
+
+            helloMap.put("name", "Freemarker");
+
+            helloTemplate.process(helloMap, stringWriter);
+
+            System.out.println(stringWriter);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/com/mongodb/HelloWordSparkStyle.java
+++ src/main/java/com/mongodb/HelloWordSparkStyle.java
@@ -1,7 +1,19 @@
 package com.mongodb;
 
-/**
- * Created by denys.novoa on 22/3/15.
- */
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
 public class HelloWordSparkStyle {
+    public static void main(String[] args) {
+
+        Spark.get("/" , new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                return "Hello Word From Spark";
+            }
+        });
+    }
 }
